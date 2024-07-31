97e6917f6a786cb93d20628e0c860e51f1b4a767
Fynn Haas <fynn.haas@ergon.ch>
2024-07-31 12:27:55 +0200

    feature/TRAINEE-1192: implement working database, implement embedded Tomcat Server It works!

--- src/main/kotlin/ch/fhaas/real_estate_rest_api/RequestHandler.kt
+++ src/main/kotlin/ch/fhaas/real_estate_rest_api/RequestHandler.kt
@@ -1,43 +1,43 @@
 package ch.fhaas.real_estate_rest_api
 
 import ch.fhaas.real_estate_rest_api.dao.RealEstateDao
-import ch.fhaas.real_estate_rest_api.entity.RealEstate
-import org.springframework.context.annotation.Bean
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.http.HttpHeaders
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean
 import org.springframework.web.bind.annotation.*
 
 
-@RequestMapping("/")
 @RestController
+@ComponentScan
 class RequestHandler(private val realEstateDao: RealEstateDao) {
 
     //TODO: Implement Requester.kt to Request Data from other API's and convert Data to own format. Add @Service Annotation
     // ETL Pipeline with *Spring Cloud Task* -> https://www.baeldung.com/spring-cloud-data-flow-etl
 
+    @RequestMapping("/")
+    fun main() {
+
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    fun helloWorld(): Any {
+        return "<h1>Test Succeeded</h1>"
+    }
+
     @GetMapping("/generated-data")
     @ResponseBody
     fun getGeneratedValues(@RequestParam("amount") amount: Int): Any {
         if (amount == 0) return HttpStatus.BAD_REQUEST
-        val list = realEstateDao.findAll().take(amount)
-        return list
+        return getListOfRealEstates(amount)
     }
+
+    private fun getListOfRealEstates(amount: Int) = realEstateDao.findAll().take(amount)
 
     @GetMapping("/real-data")
     @ResponseBody
     fun get(@RequestParam("amount") amount: Int): Any {
         if (amount == 0) return HttpStatus.BAD_REQUEST
-
         TODO("implement Requester")
     }
-
-    private fun createValidResponse(list: List<RealEstate>?): ResponseEntity<Any> {
-        val header = HttpHeaders()
-        return ResponseEntity<Any>(list, header, HttpStatus.OK)
-    }
-
 
 }
