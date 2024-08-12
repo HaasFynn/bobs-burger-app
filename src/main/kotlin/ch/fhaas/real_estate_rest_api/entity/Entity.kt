2545ea7ab6326efb4bc18e8f67df67ba124b1814
fhaas <fynngian@gmail.com>
2024-08-12 15:10:40 +0200

    feature/TRAINEE-1203: Change values of Entities again to nullable for first approach, Add JsonInclude Annotation to Entity.kt class

--- src/main/kotlin/ch/fhaas/real_estate_rest_api/entity/Entity.kt
+++ src/main/kotlin/ch/fhaas/real_estate_rest_api/entity/Entity.kt
@@ -1,5 +1,8 @@
 package ch.fhaas.real_estate_rest_api.entity
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 abstract class Entity protected constructor(
     val name: String = "",
     val wikiUrl: String = ""
