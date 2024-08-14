a4f8dd16f900df75c3a1709207e862844c23a27a
fhaas <fynngian@gmail.com>
2024-08-14 16:06:00 +0200

    feature/TRAINEE-1199: Create EpisodeService

---
+++ src/main/kotlin/ch/fhaas/real_estate_rest_api/services/entity_services/EpisodeService.kt
@@ -0,0 +1,36 @@
+package ch.fhaas.real_estate_rest_api.services.entity_services
+
+import ch.fhaas.real_estate_rest_api.entity.Episode
+import ch.fhaas.real_estate_rest_api.services.JsonReader
+import ch.fhaas.real_estate_rest_api.services.RequestClient
+import ch.fhaas.real_estate_rest_api.services.ResultHandler
+
+class EpisodeService(
+    jsonReader: JsonReader,
+    resultHandler: ResultHandler,
+    requestClient: RequestClient,
+) : EntityService<Episode>(jsonReader, resultHandler, requestClient, "episode", jsonReader::getEpisode) {
+
+    fun getByName(name: String): Episode? {
+        return get("${BASE_URL}${urlPath}?name=$name")
+    }
+
+    fun getByProductionCode(productionCode: String): Episode? {
+        return get("${BASE_URL}${urlPath}?productionCode=$productionCode")
+    }
+
+    fun getByAirDate(date: String, amount: Int = 0): List<Episode> =
+        getAmount("${BASE_URL}${urlPath}?airDate=$date", amount)
+
+    fun getBySeason(season: Int, amount: Int = 0): List<Episode> =
+        getAmount("${BASE_URL}${urlPath}?season=$season", amount)
+
+
+    fun getByIndex(num: Int): Episode? =
+        get("${BASE_URL}${urlPath}?num=$num")
+
+
+    fun getByTotalViewers(totalViewers: Int, amount: Int = 0): List<Episode> =
+        getAmount("${BASE_URL}${urlPath}?totalViewers=$totalViewers", amount)
+
+}
