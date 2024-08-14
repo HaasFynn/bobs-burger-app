b9ac55100a6d32ac02ac432ea7eba8e683e92223
fhaas <fynngian@gmail.com>
2024-08-14 17:29:56 +0200

    task/TRAINEE-1200: Implement RequestMapping template for Character & Episode

--- src/main/kotlin/ch/fhaas/real_estate_rest_api/RequestHandler.kt
+++ src/main/kotlin/ch/fhaas/real_estate_rest_api/RequestHandler.kt
@@ -1,10 +1,10 @@
 package ch.fhaas.real_estate_rest_api
 
+import ch.fhaas.real_estate_rest_api.entity.Character
+import ch.fhaas.real_estate_rest_api.entity.Episode
 import ch.fhaas.real_estate_rest_api.services.entity_services.CharacterService
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController("/")
 @ComponentScan
@@ -19,17 +19,70 @@ class RequestHandler(private val characterService: CharacterService) {
     }
 
     @RequestMapping("/")
+    @ResponseBody
     private fun main(): Any {
         return """
-            |<h1>Home Page</h1>
+            |<h1>Bob's Burger App</h1>
             |<p>To be continued...</p>
             |
         """.trimMargin()
     }
 
     @GetMapping("/test")
+    @ResponseBody
     private fun helloWorld(): Any {
         return "<h1>Test Succeeded</h1>"
     }
 
+    @GetMapping(
+        "/characters",
+        produces = ["application/json"]
+    )
+    @ResponseBody
+    private fun getCharacter(
+        @RequestParam(name = "amount", required = false)
+        amount: Int,
+        @RequestParam(name = "name", required = false)
+        name: String,
+        @RequestParam(name = "gender", required = false)
+        gender: String,
+        @RequestParam(name = "age", required = false)
+        age: Int,
+        @RequestParam(name = "hair", required = false)
+        hair: String,
+        @RequestParam(name = "currentOccupation", required = false)
+        currentOccupation: String,
+        @RequestParam(name = "occupation", required = false)
+        occupation: String,
+        @RequestParam(name = "episode", required = false)
+        episode: Int,
+        @RequestParam(name = "voiceActor", required = false)
+        voiceActor: String
+    ): Character {
+        TODO("Implement me")
+    }
+
+    @GetMapping(
+        "/episodes",
+        produces = ["application/json"]
+    )
+    @ResponseBody
+    private fun getEpisodes(
+        @RequestParam(name = "amount", required = false)
+        amount: Int,
+        @RequestParam(name = "name", required = false)
+        name: String,
+        @RequestParam(name = "productionCode", required = false)
+        productionCode: String,
+        @RequestParam(name = "airDate", required = false)
+        airDate: String,
+        @RequestParam(name = "season", required = false)
+        season: Int,
+        @RequestParam(name = "episodeNum", required = false)
+        episode: Int,
+        @RequestParam(name = "totalViewer", required = false)
+        occupation: String,
+    ): Episode {
+        TODO("Implement me")
+    }
 }
