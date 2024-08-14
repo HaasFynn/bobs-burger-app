281789eb44774fac7f629499d23fcfaf6f423f46
fhaas <fynngian@gmail.com>
2024-08-14 16:12:29 +0200

    feature/TRAINEE-1199: Remove EpisodeService.getByName() because Episode has no Name

--- src/main/kotlin/ch/fhaas/real_estate_rest_api/services/entity_services/EpisodeService.kt
+++ src/main/kotlin/ch/fhaas/real_estate_rest_api/services/entity_services/EpisodeService.kt
@@ -4,16 +4,14 @@ import ch.fhaas.real_estate_rest_api.entity.Episode
 import ch.fhaas.real_estate_rest_api.services.JsonReader
 import ch.fhaas.real_estate_rest_api.services.RequestClient
 import ch.fhaas.real_estate_rest_api.services.ResultHandler
+import org.springframework.stereotype.Service
 
+@Service
 class EpisodeService(
     jsonReader: JsonReader,
     resultHandler: ResultHandler,
     requestClient: RequestClient,
 ) : EntityService<Episode>(jsonReader, resultHandler, requestClient, "episode", jsonReader::getEpisode) {
-
-    fun getByName(name: String): Episode? {
-        return get("${BASE_URL}${urlPath}?name=$name")
-    }
 
     fun getByProductionCode(productionCode: String): Episode? {
         return get("${BASE_URL}${urlPath}?productionCode=$productionCode")
