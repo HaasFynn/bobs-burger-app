e46c123ff851fc0f597900d5344657622337e8b6
fhaas <fynngian@gmail.com>
2024-08-22 15:28:13 +0200

    fix/services: Implement Services

--- src/main/kotlin/ch/fhaas/bobs_burger_api/services/entity_services/CharacterService.kt
+++ src/main/kotlin/ch/fhaas/bobs_burger_api/services/entity_services/CharacterService.kt
@@ -1,31 +1,35 @@
 package ch.fhaas.bobs_burger_api.services.entity_services
 
 import ch.fhaas.bobs_burger_api.entity.VoiceActor
+import ch.fhaas.bobs_burger_api.services.RequestBuilder
 import ch.fhaas.bobs_burger_api.services.RequestClient
 import kong.unirest.core.json.JSONArray
 import kong.unirest.core.json.JSONObject
 import org.springframework.stereotype.Service
 
 @Service
-class CharacterService(requestClient: RequestClient) : EntityService(requestClient, "characters") {
+class CharacterService(
+    requestClient: RequestClient,
+    requestBuilder: RequestBuilder
+) : EntityService(requestClient, requestBuilder, "characters") {
 
     fun getByName(name: String): JSONObject {
-        val debug = get("?name=$name", 0).getJSONObject(0)
+        val debug = get(Pair("name", name), 0).getJSONObject(0)
         return debug
     }
 
     fun getByGender(gender: String, amount: Int = 0): JSONArray =
-        get("?gender=$gender", amount)
+        get(Pair("gender", gender), amount)
 
     fun getByAge(age: Int, amount: Int = 0): JSONArray =
-        get("?age=$age", amount)
+        get(Pair("age", age), amount)
 
     fun getByHair(hair: String, amount: Int = 0): JSONArray =
-        get("?hair=$hair", amount)
+        get(Pair("hair", hair), amount)
 
     fun getByOccupation(occupation: String, amount: Int = 0): JSONArray =
-        get("?occupation=$occupation", amount)
+        get(Pair("occupation", occupation), amount)
 
     fun getByVoiceActor(voiceActor: VoiceActor, amount: Int = 0): JSONArray =
-        get("?voicedBy=${voiceActor.name}", amount)
+        get(Pair("voiceActor", voiceActor.name), amount)
 }
--- src/main/kotlin/ch/fhaas/bobs_burger_api/services/entity_services/EntityService.kt
+++ src/main/kotlin/ch/fhaas/bobs_burger_api/services/entity_services/EntityService.kt
@@ -1,36 +1,24 @@
 package ch.fhaas.bobs_burger_api.services.entity_services
 
+import ch.fhaas.bobs_burger_api.services.RequestBuilder
 import ch.fhaas.bobs_burger_api.services.RequestClient
 import kong.unirest.core.JsonNode
 import kong.unirest.core.json.JSONArray
 
 abstract class EntityService(
     private val requestClient: RequestClient,
-    private val urlPath: String
+    private val requestBuilder: RequestBuilder,
+    private val directory: String
 ) {
-    companion object {
-        @JvmStatic
-        private val BASE_URL: String = "https://bobsburgers-api.herokuapp.com/"
-    }
 
-    protected fun get(parameter: String, amount: Int = 0): JSONArray {
-        val url: String = buildUrl(amount, arrayOf(parameter))
+    protected fun get(parameter: Pair<String, Any>, amount: Int = 0): JSONArray {
+        val url: String = buildUrl(amount, parameter)
         return request(url).array
     }
 
     private fun request(url: String): JsonNode = requestClient.request(url)
 
-    private fun buildUrl(amount: Int = 0, params: Array<String>): String =
-        BASE_URL +
-                urlPath +
-                parametersToString(params) +
-                getLimit(amount)
-
-    private fun parametersToString(params: Array<String>): String {
-        return params.fold(StringBuilder()) { string, param ->
-            return string.append(param).toString()
-        }.toString()
-    }
+    private fun buildUrl(amount: Int = 0, params:  Pair<String, Any>): String = requestBuilder.buildUrl(directory, mapOf(params)) + getLimit(amount)
 
     private fun getLimit(amount: Int) = if (amount > 0) "?limit=$amount" else ""
 }
